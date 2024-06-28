5b448929cd7455a0a275cac3a9cbca6190a25aef
giu-giu <195412063@qq.com>
2024-06-29 06:52:38 +0800

    fix oled driver, bug not compile ok.

--- software/src/inc/driver/oled.h
+++ software/src/inc/driver/oled.h
@@ -17,13 +17,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 #ifndef __OLED_H
 #define __OLED_H
-
-#include "stdlib.h"
-#include "delay.h"
 #include "sys.h"
 #include "stdlib.h"
-#include "iic.h"
-
 #define OLED_MODE 0
 #define SIZE 8
 #define XLevelL		0x00
@@ -35,11 +30,11 @@
 #define Y_WIDTH 	64
 //-----------------OLED IIC端口定义----------------
 
-#define OLED_SCLK_Clr() GPIO_ResetBits(GPIOA,GPIO_Pin_5)//SCL
-#define OLED_SCLK_Set() GPIO_SetBits(GPIOA,GPIO_Pin_5)
+#define OLED_SCLK_Clr() GPIO_ResetBits(GPIOB,GPIO_Pin_10)//SCL
+#define OLED_SCLK_Set() GPIO_SetBits(GPIOB,GPIO_Pin_10)
 
-#define OLED_SDIN_Clr() GPIO_ResetBits(GPIOA,GPIO_Pin_7)//SDA
-#define OLED_SDIN_Set() GPIO_SetBits(GPIOA,GPIO_Pin_7)
+#define OLED_SDIN_Clr() GPIO_ResetBits(GPIOB,GPIO_Pin_11)//SDA
+#define OLED_SDIN_Set() GPIO_SetBits(GPIOB,GPIO_Pin_11)
 
 
 #define OLED_CMD  0	//写命令
@@ -49,6 +44,7 @@ extern const unsigned char F6x8[][6];
 extern const unsigned char F8X16[];
 extern const char Hzk[][32];
 extern const unsigned char BMP1[];
+
 
 //OLED控制用函数
 void OLED_WR_Byte(unsigned dat,unsigned cmd);
@@ -60,19 +56,23 @@ void OLED_DrawPoint(u8 x,u8 y,u8 t);
 void OLED_Fill(u8 x1,u8 y1,u8 x2,u8 y2,u8 dot);
 void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 Char_Size);
 void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 size);
-void OLED_ShowString(u8 x,u8 y, u8 *p,u8 Char_Size);
+void OLED_ShowString(u8 x,u8 y, char *p,u8 Char_Size);
 void OLED_Set_Pos(unsigned char x, unsigned char y);
 void OLED_ShowCHinese(u8 x,u8 y,u8 no);
-void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[]);
+void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1, const unsigned char BMP[]);
 void Delay_50ms(unsigned int Del_50ms);
 void Delay_1ms(unsigned int Del_1ms);
 void fill_picture(unsigned char fill_Data);
 void Picture();
-// void IIC_Start();
-// void IIC_Stop();
-// void IIC_Wait_Ack();
+void IIC_Start();
+void IIC_Stop();
 void Write_IIC_Command(unsigned char IIC_Command);
 void Write_IIC_Data(unsigned char IIC_Data);
 void Write_IIC_Byte(unsigned char IIC_Byte);
 
+void IIC_Wait_Ack();
 #endif
+
+
+
+
--- software/src/src/arm_roboot_executor.c
+++ software/src/src/arm_roboot_executor.c
@@ -13,13 +13,7 @@
 
 void init_roboot_state()
 {
-    pca_init(60);
-    servo_init(SERVO_1, SERVO_RANGE_1_INIT);
-    servo_init(SERVO_2, SERVO_RANGE_2_INIT);
-    servo_init(SERVO_3, SERVO_RANGE_3_INIT);
-    servo_init(SERVO_4, SERVO_RANGE_4_INIT);
-    servo_init(SERVO_5, SERVO_RANGE_5_INIT);
-    servo_init(SERVO_6, SERVO_RANGE_6_INIT);
+
 }
 
 void update_roboot_state(struct command_context *command_context)
