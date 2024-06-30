8f7d9d92c9abc55fa93608ff4ab0a630f855a707
giu-giu <195412063@qq.com>
2024-07-01 07:44:45 +0800

    fix to display gyro value.

--- software/src/src/led_display.c
+++ software/src/src/led_display.c
@@ -10,7 +10,9 @@
 
 #include <led_display.h>
 
-struct gimbal_info gimbal_info = {0, 0, 0};
+struct gimbal_info gimbal_info = {0, 0, -12300};
+
+const uchar PIXEL_DISTANCE_INTERVAL = 6;
 
 void init_led_display()
 {
@@ -23,31 +25,45 @@ void display_info(struct command_context *command_context)
 
 }
 
+void show_number_by_order(u8 x, u8 y, short number)
+{
+
+}
+
 void show_short_number(u8 x, u8 y, short number)
 {
     if (number == 0)
     {
-        OLED_ShowChar(x + 8, y, 0x30, 8);
+        OLED_ShowChar(x + PIXEL_DISTANCE_INTERVAL, y, 0x30, 8);
+        return;
     }
 
     short loop_number = number;
+    if (number < 0)
+    {
+        OLED_ShowChar(x + PIXEL_DISTANCE_INTERVAL, y, '-', 8);
+        loop_number = ~(number - 1);
+        x += PIXEL_DISTANCE_INTERVAL;
+    }
+
     char show_numbers[5] = {0};
     char remainder = loop_number % 10;
 
     uchar loop_idx = 4;
-    while(remainder > 0 && loop_idx >= 0)
+    do
     {
         show_numbers[loop_idx] = remainder;
         loop_idx--;
         loop_number /= 10;
         remainder = loop_number % 10;
-    }
+    } while(loop_number > 0 && loop_idx >= 0);
 
-    for (int i = 0; i < 5; i++)
+    for (uchar i = 0,flag = 0; i < 5; i++)
     {
-        if (show_numbers[i] != 0)
+        if (show_numbers[i] != 0 || flag > 0)
         {
-            OLED_ShowChar(x + (i * 6), y, show_numbers[i] + 0x30, 8);
+            OLED_ShowChar(x + ((flag + 1) * PIXEL_DISTANCE_INTERVAL), y, show_numbers[i] + 0x30, 8);
+            flag++;
         }
     }
 }
@@ -67,9 +83,9 @@ void show_gimbal_info(struct gimbal_info *gimbal_info)
     OLED_ShowString(20, 5, "Y: ", 8);
     OLED_ShowString(20, 6, "Z: ", 8);
 
-    show_short_number(32, 4, gimbal_info->gyro_x);
-    show_short_number(32, 5, gimbal_info->gyro_y);
-    show_short_number(32, 6, gimbal_info->gyro_z);
+    show_short_number(38, 4, gimbal_info->gyro_x);
+    show_short_number(38, 5, gimbal_info->gyro_y);
+    show_short_number(38, 6, gimbal_info->gyro_z);
 }
 
 const struct module_command_executor led_display_executor = {init_led_display, display_info};
