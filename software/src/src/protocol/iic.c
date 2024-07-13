a2296398a26c035938d258d588427ebfb108154d
giu-giu <195412063@qq.com>
2024-07-13 15:40:40 +0800

    fix iic gpio output from pp to od.

--- software/src/src/protocol/iic.c
+++ software/src/src/protocol/iic.c
@@ -17,7 +17,7 @@ void IIC_Init(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); //使能GPIOB时钟
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; //推挽输出
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD; //推挽输出
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
     GPIO_SetBits(GPIOB, GPIO_Pin_10 | GPIO_Pin_11); //PB10,PB11 输出高
